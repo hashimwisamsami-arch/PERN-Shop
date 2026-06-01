bb27455992987a68bb8968af4f23be0700a8e707
Hashim <hashimwisamsami@email.com>
2026-06-01 16:16:26 +0300

    made clerk middleware in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+
 const app = express();
+app.use(clerkMiddleware()); //auth object will be attached to the req
+app.use(express.json()); //parses json req bodies
+app.use(express.urlencoded({ extended: true })); //parses from data(like HTML forms)
 
 app.get("/", (req, res) => {
   res.json({
