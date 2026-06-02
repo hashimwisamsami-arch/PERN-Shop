8114bd67b9e99c7e2a36f00d643504b9f7fdd35d
Hashim <hashimwisamsami@email.com>
2026-06-02 17:36:28 +0300

    fixed bug in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,9 @@ import App from "./App.jsx";
 import "./index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key (VITE_CLERK_PUBLISHABLE_KEY)");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
